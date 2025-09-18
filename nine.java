2f31601513f22b839e7543f3243ff417e4420274
dipak saket <saketdeepak009@gmail.com>
2025-09-18 15:15:46 +0530

    firstcommit

---
+++ nine.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class nine {
+    public static void main(String[] args) {
+        
+        float a = 7/4.0f*9/2.0f;
+        System.out.println(a);
+
+        char grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+
+
+        // Decrypting the grade
+
+
+
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+
+     Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+     System.out.println(n>8);
+
+
+     System.out.println(7*49/7+35/5);
+
+    
+
+    }
+    
+}
