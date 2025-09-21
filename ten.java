7eb70edbe01b926b8cf7319abe6dfb9f0f708098
dipak saket <saketdeepak009@gmail.com>
2025-09-21 22:07:52 +0530

    firstcommit

---
+++ ten.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ten {
+
+    public static void main(String[] args) {
+
+       // String name = new String("Harry");
+
+       String name = "harry";
+
+        System.out.println(name);
+        System .out.print("the name is:");
+        System.out.print(name);
+
+        int a = 16;
+        float b = 15.5f;
+        System.out.printf("the valuen of a is %d and the value of b is %f ",a,b );
+       // System.out.format("the valuen of a is %d and the value of b is %f ",a,b );
+
+       Scanner sc = new Scanner(System.in);
+
+       String st = sc.nextLine();
+       System.out.println(st);
+
+
+    }
+    
+}
